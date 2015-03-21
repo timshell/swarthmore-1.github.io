2ff36b42ab2802117100a494d9a4669bbf2f2526
timshell <magrawa1@swarthmore.edu>
2015-03-21 14:04:28 -0400

    Added a haiku

--- poem.js
+++ poem.js
@@ -6,6 +6,12 @@ function nextLine(line) {
 "<p onClick=nextLine(4)>Now they all send <em>me</em> changes, d'oh!</p>",
 "<p onClick=nextLine(5)>Said the maintainer, 'Welcome to the club!'</p>");
 
+		var haiku = new Array("<p onClick=nextLine(1)>Here is a haiku</p>",
+"<p onClick=nextLine(2)>This is seven syllabus</p>",
+"<p onClick=nextLine(3)>And this is the end.</p>");
+
 	document.getElementById("line").innerHTML=poem[line];
+
+	document.getElementById("line").innerHTML=haiku[line];
 }
 
